6586d5a458ac45a8aa0f09db5eb5f48d3b9e044d
Daniel Windham <dewindham@gmail.com>
2016-08-07 12:24:20 -0700

    pv: add `equals` method to compare PVs by value

--- src/com/github/tenedor/paxos/PaxosValue.java
+++ src/com/github/tenedor/paxos/PaxosValue.java
@@ -10,4 +10,38 @@ public class PaxosValue {
     return "PaxosValue [key=" + key + ", value=" + value + ", requestId=" +
         requestId + "]";
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((key == null) ? 0 : key.hashCode());
+    result = prime * result + requestId;
+    result = prime * result + ((value == null) ? 0 : value.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    PaxosValue other = (PaxosValue) obj;
+    if (key == null) {
+      if (other.key != null)
+        return false;
+    } else if (!key.equals(other.key))
+      return false;
+    if (requestId != other.requestId)
+      return false;
+    if (value == null) {
+      if (other.value != null)
+        return false;
+    } else if (!value.equals(other.value))
+      return false;
+    return true;
+  }
 }

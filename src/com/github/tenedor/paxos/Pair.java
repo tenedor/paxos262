e1d4e6d32205021797f3ceac085d03604afa83ba
Daniel Windham <dewindham@gmail.com>
2016-08-07 18:35:55 -0700

    lEntry, pls, pair: stylefix: use 2-space indents

--- src/com/github/tenedor/paxos/Pair.java
+++ src/com/github/tenedor/paxos/Pair.java
@@ -12,58 +12,58 @@ package com.github.tenedor.paxos;
  */
 public class Pair<A, B> implements Comparable<Pair<A, B>> {
 
-    public final A first;
-    public final B second;
+  public final A first;
+  public final B second;
 
-    private Pair(A first, B second) {
-        this.first = first;
-        this.second = second;
-    }
+  private Pair(A first, B second) {
+    this.first = first;
+    this.second = second;
+  }
 
-    public static <A, B> Pair<A, B> of(A first, B second) {
-        return new Pair<A, B>(first, second);
-    }
+  public static <A, B> Pair<A, B> of(A first, B second) {
+    return new Pair<A, B>(first, second);
+  }
 
-    @Override
-    public int compareTo(Pair<A, B> o) {
-        int cmp = compare(first, o.first);
-        return cmp == 0 ? compare(second, o.second) : cmp;
-    }
+  @Override
+  public int compareTo(Pair<A, B> o) {
+    int cmp = compare(first, o.first);
+    return cmp == 0 ? compare(second, o.second) : cmp;
+  }
 
-    // todo move this to a helper class.
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    private static int compare(Object o1, Object o2) {
-        return o1 == null ? o2 == null ? 0 : -1 : o2 == null ? +1
-                : ((Comparable) o1).compareTo(o2);
-    }
+  // todo move this to a helper class.
+  @SuppressWarnings({ "unchecked", "rawtypes" })
+  private static int compare(Object o1, Object o2) {
+    return o1 == null ? o2 == null ? 0 : -1 : o2 == null ? +1
+        : ((Comparable) o1).compareTo(o2);
+  }
 
-    @Override
-    public int hashCode() {
-        return (hashcode(first) * 65497) ^ hashcode(second);
-    }
+  @Override
+  public int hashCode() {
+    return (hashcode(first) * 65497) ^ hashcode(second);
+  }
 
-    // todo move this to a helper class.
-    private static int hashcode(Object o) {
-        return o == null ? 0 : o.hashCode();
-    }
+  // todo move this to a helper class.
+  private static int hashcode(Object o) {
+    return o == null ? 0 : o.hashCode();
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Pair<?, ?>))
-            return false;
-        if (this == obj)
-            return true;
-        return equal(first, ((Pair<?, ?>) obj).first)
-                && equal(second, ((Pair<?, ?>) obj).second);
-    }
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Pair<?, ?>))
+      return false;
+    if (this == obj)
+      return true;
+    return equal(first, ((Pair<?, ?>) obj).first)
+        && equal(second, ((Pair<?, ?>) obj).second);
+  }
 
-    // todo move this to a helper class.
-    private boolean equal(Object o1, Object o2) {
-        return o1 == null ? o2 == null : (o1 == o2 || o1.equals(o2));
-    }
+  // todo move this to a helper class.
+  private boolean equal(Object o1, Object o2) {
+    return o1 == null ? o2 == null : (o1 == o2 || o1.equals(o2));
+  }
 
-    @Override
-    public String toString() {
-        return "(" + first + ", " + second + ')';
-    }
+  @Override
+  public String toString() {
+    return "(" + first + ", " + second + ')';
+  }
 }
--- src/com/github/tenedor/paxos/PaxosLeaderState.java
+++ src/com/github/tenedor/paxos/PaxosLeaderState.java
@@ -3,11 +3,16 @@ package com.github.tenedor.paxos;
 import java.util.*;
 
 public class PaxosLeaderState extends PaxosState {
-	  public int precedentLeaderEra;
-	  public PaxosValue precedentValue;
-	  Set<PaxosDb> acceptors;
+  public int precedentLeaderEra;
+  public PaxosValue precedentValue;
+  Set<PaxosDb> acceptors;
 
-	  public PaxosLeaderState() {
-	    acceptors = new HashSet<PaxosDb>();
-	  }
+  /**
+   * version uid for serializability
+   */
+  private static final long serialVersionUID = -7474284782970134806L;
+
+  public PaxosLeaderState() {
+    acceptors = new HashSet<PaxosDb>();
+  }
 }
